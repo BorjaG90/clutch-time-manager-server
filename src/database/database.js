c8a02c6c2fda658d28fac63d22bffd71cc401f21
BorjaG90 <borjag90dev@gmail.com>
2020-09-22 11:16:00 +0200

    feat: Add connection string #2

---
+++ src/database/database.js
@@ -0,0 +1,22 @@
+import Sequelize from 'sequelize';
+
+import { database } from './keys';
+
+module.exports = new Sequelize(
+  database.database,
+  database.user,
+  database.password,
+  {
+    host: database.host,
+    dialect: database.dialect,
+    operatorAliases: false,
+
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    logging: false
+  }
+);
