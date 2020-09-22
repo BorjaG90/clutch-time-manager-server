d58a977bd84cb593778c4675ee576fc9b1376dcd
BorjaG90 <borjag90dev@gmail.com>
2020-09-22 12:41:23 +0200

    feat: Add basic User routes by Id

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express'
-import { createUser, getUsers } from '../controllers/user.controller'
+import { createUser, deleteUser, getOneUser, getUsers, updateUser } from '../controllers/user.controller'
 
 const router = Router();
 
 // /api/users/
 router.post('/', createUser);
 router.get('/', getUsers);
+
+// api/users/:userID
+router.get('/:id', getOneUser);
+router.delete('/:id', deleteUser);
+router.put('/:id', updateUser);
 
 export default router;
