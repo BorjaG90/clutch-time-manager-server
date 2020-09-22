2ce3d35147cb39740cb55c99b403849f3b17fcb8
BorjaG90 <borjag90dev@gmail.com>
2020-09-22 09:46:37 +0200

    chore: Use index.js instead app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-'use strict'
+import express, {json} from 'express';
+import morgan from 'morgan';
 
 require('dotenv').config();
-const express = require('express');
 
 let app = express();
 
+// Middlewares
+app.use(morgan('dev'));
+app.use(json());
+
+// Init server
 let port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log(`ClutchTime Manager server listening in port: ${port}`);
 });
+
+export default app;
